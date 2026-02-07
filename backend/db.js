f4dd0445cf7d1a26a85c8b592dbbca19e6cf0ab9
Rameshmamuduru <rameshmamuduru7799@gmail.com>
2026-02-07 16:39:23 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,24 @@
-const mysql=require("mysql2");
-const db=mysql.createConnection({host:"localhost",user:"root",password:"password",database:"shopdb"});
-db.connect(err=>{if(err)throw err;console.log("MySQL Connected");});module.exports=db;
+const mysql = require("mysql2");
+
+const config = {
+  host: "mysql",          // service name from docker-compose
+  user: "appuser",
+  password: "rootpass",
+  database: "shopdb"
+};
+
+function connectWithRetry() {
+  const db = mysql.createConnection(config);
+
+  db.connect(err => {
+    if (err) {
+      console.error("MySQL not ready... retrying in 5 seconds");
+      setTimeout(connectWithRetry, 5000);
+    } else {
+      console.log("MySQL Connected");
+      module.exports = db;
+    }
+  });
+}
+
+connectWithRetry();
